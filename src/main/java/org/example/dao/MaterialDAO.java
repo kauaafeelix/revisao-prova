11c810d7a4f51d2c72cef8f5b222b4338621cda5
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-27 20:46:28 -0300

    feat: adiciona metodo para listar materiais

--- src/main/java/org/example/dao/MaterialDAO.java
+++ src/main/java/org/example/dao/MaterialDAO.java
@@ -7,10 +7,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MaterialDAO {
 
-    public boolean existeNomeMaterial (String nome) throws SQLException {
+    public boolean existeNomeMaterial(String nome) throws SQLException {
 
         String sql = """
                 SELECT nome
@@ -18,19 +20,19 @@ public class MaterialDAO {
                 WHERE nome = ?
                 """;
 
-        try(Connection conn = Conexao.conectar();
-            PreparedStatement ps = conn.prepareStatement(sql)) {
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, nome);
 
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
                 return true;
+            }
         }
-    }
         return false;
     }
 
-    public void cadastrarMaterial (Material material) throws SQLException{
+    public void cadastrarMaterial(Material material) throws SQLException {
         String sql = """
                 INSERT INTO Material (
                 nome,
@@ -39,12 +41,37 @@ public class MaterialDAO {
                 VALUES (?,?,?)
                 """;
 
-        try(Connection conn = Conexao.conectar();
-            PreparedStatement ps = conn.prepareStatement(sql)) {
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, material.getNome());
             ps.setString(2, material.getUnidade());
             ps.setDouble(3, material.getEstoque());
             ps.executeUpdate();
         }
+    }
+
+    public List<Material> listarMateriais() throws SQLException {
+        List<Material> materiais = new ArrayList<>();
+
+        String sql = """
+                SELECT id, nome, unidade, estoque
+                FROM Material
+                """;
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Material material = new Material(
+                        rs.getInt("id"),
+                        rs.getString("nome"),
+                        rs.getString("unidade"),
+                        rs.getDouble("estoque")
+                );
+                materiais.add(material);
+            }
+        }
+        return materiais;
     }
 }
