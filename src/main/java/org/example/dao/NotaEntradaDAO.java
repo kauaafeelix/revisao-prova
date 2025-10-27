672eca1c58d8d5e6e123b8b46b62b343978ae606
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-27 20:46:34 -0300

    feat: adiciona método para cadastrar nota de entrada e associar materiais

--- src/main/java/org/example/dao/NotaEntradaDAO.java
+++ src/main/java/org/example/dao/NotaEntradaDAO.java
@@ -3,14 +3,13 @@ package org.example.dao;
 import org.example.Conexao.Conexao;
 import org.example.model.NotaEntrada;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.LocalDate;
+import java.util.HashMap;
 
 public class NotaEntradaDAO {
 
-    public void cadastrarNotaEntrada(NotaEntrada notaEntrada)throws SQLException{
+    public int cadastrarNotaEntrada(NotaEntrada notaEntrada) throws SQLException {
 
         String sql = """
                 INSERT INTO NotaEntrada (idFornecedor, dataEntrada)
@@ -18,10 +17,36 @@ public class NotaEntradaDAO {
                 """;
 
         try (Connection conn = Conexao.conectar();
-             PreparedStatement ps = conn.prepareStatement(sql)){
+             PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
 
             ps.setInt(1, notaEntrada.getIdFornecedor());
             ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                int idGerado = rs.getInt(1);
+                notaEntrada.setId(idGerado);
+                return idGerado;
+            }
+        }
+        return 0;
+    }
+
+    public void associarMateriais(HashMap<Integer, Double> estoque, int idFornecedor) throws SQLException {
+        String sql = """
+                INSERT INTO NotaEntrada (idNotaEntrada, idMaterial, quantidade)
+                VALUES (?,?,?);
+                """;
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            for (Integer idMaterial : estoque.keySet()) {
+                ps.setInt(1, idFornecedor);
+                ps.setInt(2, idMaterial);
+                ps.setDouble(3, estoque.get(idMaterial));
+                ps.executeUpdate();
+            }
         }
     }
 }
